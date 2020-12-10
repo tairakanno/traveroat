4bcb3a33f28ba43ce470ab66e685d79f61450019
tairakanno <tairakannotaira@gmail.com>
2020-12-10 23:21:46 +0900

    新規投稿機能実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to posts_path
+    else
+      render action: :new
+    end
+  end
+  def index
+    
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title,:text).merge(user_id: current_user.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :videos
   devise_for :users
   root to: 'tops#index'
-  
+  resources :posts
 end
