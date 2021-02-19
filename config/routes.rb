aabcbeffce7c29bf9df6efe32a44d10a065ecf49
tairakanno <tairakannotaira@gmail.com>
2021-02-19 23:56:03 +0900

    いいね機能ルーティング 作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :videos
   devise_for :users
   root to: 'tops#index'
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
   resources :users
 end
